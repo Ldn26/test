151f232a5ebe092759e8c68936bfa4aede1f2d7d
Ldn26 <youcefldn26@gmail.com>
2026-08-08 10:42:01 +0200

    add url

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -12,7 +12,7 @@ const [users, setUsers] = useState([]);
       onClick={async () => {
         try {
           // 10.103.74.226
-          const response = await fetch("http://99.103.74.226:4000/users");
+          const response = await fetch("http://10.108.142.73:4000/users");
           if (!response.ok) {
             throw new Error("Network response was not ok");
           }
